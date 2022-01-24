3b85f250dbf3f9f186ed836548202d1da292b4f3
Bohdan <bohdan.kornatskyi@gmail.com>
2022-01-23 21:08:14 -0800

    messuring execution time for arithmetical functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <bitset>
+#include <chrono>
 #include <iostream>
 #include <math.h>
 
@@ -10,8 +11,48 @@
 
 #include <stdio.h>
 
+void messureExecutionTime(const std::function<void()> &function,
+                          std::string description) {
+  using std::chrono::duration;
+  using std::chrono::duration_cast;
+  using std::chrono::high_resolution_clock;
+  using std::chrono::milliseconds;
+
+  auto t1 = high_resolution_clock::now();
+  function();
+  auto t2 = high_resolution_clock::now();
+
+  /* Getting number of milliseconds as an integer. */
+  auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+  /* Getting number of milliseconds as a double. */
+  duration<double, std::milli> ms_double = t2 - t1;
+
+  std::cout << description << std::endl;
+  std::cout << ms_int.count() << "ms\n";
+  std::cout << ms_double.count() << "ms\n";
+}
+
 int main(int, char **) {
 
-  std::cout << "Main function in src/" << std::endl;
+  BigInt n1 =
+      "3567983423927357293759823795345734769347678348758723759772340238";
+
+  BigInt n2 = "2375977234023840234232349078503479857349758934798578934735679834"
+              "23927357";
+
+  auto addition = [=]() { n1 + n2; };
+  auto substruction = [=]() { n1 - n2; };
+  auto division = [=]() { n1 / n2; };
+  auto multiplication = [=]() { n1 *n2; };
+  auto modulus = [=]() { BigInt("123"); };
+
+  messureExecutionTime(addition, (n1.toString() + " + " + n2.toString()));
+  messureExecutionTime(substruction, (n1.toString() + " - " + n2.toString()));
+  messureExecutionTime(multiplication, (n1.toString() + " * " + n2.toString()));
+  messureExecutionTime(division, (n1.toString() + " / " + n2.toString()));
+  messureExecutionTime(modulus, (n1.toString() + " % " + n2.toString()));
+
+  // std::cout << "Main function in src/" << std::endl;
   return 0;
 }
